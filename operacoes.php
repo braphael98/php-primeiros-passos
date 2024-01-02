bb58ed9a7efed585e27a35c767540371474778b5
bruno <braphaelperez98@gmail.com>
2024-01-02 15:52:18 -0300

    Operações e tipos

---
+++ operacoes.php
@@ -0,0 +1,14 @@
+<?php
+
+$soma = 2 + 2;
+$sub = 2 - 1;
+$mult = 2 * 2;
+$div = 2 / 2;
+
+
+$doisAoCubo = 2 ** 3;
+
+$restoDiv = 10 % 3; 
+
+echo $restoDiv;
+
---
+++ tipos.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$idade = 21;
+$salario = 1000.50;
+$divisao = 10/3;
+$texto = "Banana";
+$verdadeiro = true;
+$falso = false;
+
+//gettype para saber o tipo do tipo da variavel.
+
+echo gettype($falso);
+
