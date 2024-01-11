3e02d93677d4851390a74a8da2a04ee744b6fa42
bruno <braphaelperez98@gmail.com>
2024-01-11 15:56:23 -0300

    fixed

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,6 +1,6 @@
 <?php
 
-function sacar($conta, $valorASacar)
+function sacar(array $conta, float $valorASacar): array
 {
     if ($valorASacar > $conta['saldo']) {
         exibeMensagem("Você não pode sacar este valor");
@@ -9,6 +9,17 @@ function sacar($conta, $valorASacar)
     }
     return $conta;
 }
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
 function exibeMensagem($mensagem)
 {
     echo $mensagem . PHP_EOL;
@@ -27,7 +38,7 @@ $contasCorrentes = [
     ],
     '055.212.432-21' => [
         'titular' => 'Pedro da sailva',
-        'saldo' => 1254,
+        'saldo' => 1000,
     ],
     '012.321.534-44' => [
         'titular' => 'Claudia dos santos',
@@ -36,9 +47,11 @@ $contasCorrentes = [
 
 ];
 
-$contasCorrentes['023.320.450-79'] = sacar($contasCorrentes['023.320.450-79'], 500);
+$contasCorrentes['023.320.450-79'] = sacar($contasCorrentes['023.320.450-79'], 100);
 
-$contasCorrentes['123.456.789.10'] = sacar($contasCorrentes['123.456.789.10'], 500);
+$contasCorrentes['123.456.789.10'] = sacar($contasCorrentes['123.456.789.10'], 100);
+
+$contasCorrentes['055.212.432-21'] = depositar($contasCorrentes['055.212.432-21'], 200);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta['titular'] . " " . $conta['saldo']);
